9806355b3237843e8790d0688a21f22e5cd12f67
Ellie Hastings <elliehastings@gmail.com>
2023-08-19 10:13:07 -0400

    Set up initial endpoint and date validation

---
+++ README.md
@@ -0,0 +1,35 @@
+# About this app
+
+TODO - explainer!
+
+# TODO
+
+Tracking project todos:
+
+1.0 (proof of concept)
+
+* Create endpoint
+* Add BeautifulSoup
+* Pull Table
+* Pull Fixtures
+* Structure data to send to GPT
+* Call GPT to get recommendations
+* Return formatted recommendations
+
+2.0
+
+* Add models and persistent storage using SQLite
+* Add background task to scrape table & fixtures for given dates once per day
+* Cache API results so we're not webscraping once per API request like monsters
+
+3.0
+
+* Add simple UI (defaulting to requesting the next few days)
+
+4.0
+
+* envs/Docker/deploy
+
+5.0
+
+* Add custom UI with date filtering or other filters (league, etc)
--- main.py
+++ main.py
@@ -1,8 +1,33 @@
-from fastapi import FastAPI
+from datetime import date, timedelta
+from typing import Union, Annotated
+
+from fastapi import FastAPI, HTTPException, Query
+
+from starlette.responses import RedirectResponse
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    response = RedirectResponse(url='/fixture-recommendations')
+    return response
+
+
+@app.get("/fixture-recommendations")
+async def get_fixture_recommendations(
+    start_date: Annotated[Union[date, None], Query()] = None,
+    end_date: Annotated[Union[date, None], Query()] = None
+):
+    DEFAULT_DAY_RANGE = 3
+
+    if start_date is None:
+        start_date = date.today()
+    if end_date is None:
+        end_date = date.today() + timedelta(days=DEFAULT_DAY_RANGE)
+
+    if start_date < date.today() or end_date < date.today() or end_date < start_date:
+        raise HTTPException(
+            status_code=404, detail="Dates must be in present or future")
+
+    return {"start_date": start_date, "end_date": end_date}
